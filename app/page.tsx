f7576477de2b4c52afb49eeb35853f7c5a41e571
shrutisharma7500 <shrutisharma94827@gmail.com>
2025-11-02 18:06:43 +0530

    Initial commit: setup Aivora AI video call project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-amber-950  font-extrabold text-5xl">
+    <Button>
+Click me
+    </Button>
+    </div>
+  );
+}
